8178c5fc82eb6a216f048a92509af285b45be23d
quentinm73 <67158466+quentinm73@users.noreply.github.com>
2023-10-20 10:26:36 +0200

    Add files via upload

--- tp1exo5.py
+++ tp1exo5.py
@@ -1,9 +1,3 @@
-Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import random
->>> valeur=random.randint(0, 100)
->>> print(valeur)
-33
->>> print(valeur)
-33
->>> 
+import random
+valeur=random.randint(0, 100)
+print(valeur)
