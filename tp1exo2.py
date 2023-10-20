3cee95abfd55b6f4dcba4ce3236f343ac7989d7c
quentinm73 <67158466+quentinm73@users.noreply.github.com>
2023-10-20 10:25:39 +0200

    Add files via upload

--- tp1exo2.py
+++ tp1exo2.py
@@ -1,9 +1,5 @@
-Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> jour = 20
->>> heure = 10
->>> minute = 0
->>> nb_min = 19 * (60*24) + (10*60)
->>> print(nb_min)
-27960
->>> 
+jour = 20
+heure = 10
+minute = 0
+nb_min = 19 * (60*24) + (10*60)
+print(nb_min)
