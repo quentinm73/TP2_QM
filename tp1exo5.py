e2930766b36f18f0ab7d1064d26d4a9dfadf6854
quentinm73 <67158466+quentinm73@users.noreply.github.com>
2023-10-20 09:52:22 +0200

    Add files via upload
    Exercice 5 TP1

---
+++ tp1exo5.py
@@ -0,0 +1,9 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import random
+>>> valeur=random.randint(0, 100)
+>>> print(valeur)
+33
+>>> print(valeur)
+33
+>>> 
