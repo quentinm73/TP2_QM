12b2d60ab15b4cfc014af34208bfeae10d0327d1
quentinm73 <67158466+quentinm73@users.noreply.github.com>
2023-10-26 10:51:17 +0200

    Create TP2_exo8.py

---
+++ TP2_exo8.py
@@ -0,0 +1,7 @@
+x = float(input("Entrez une valeur :"))
+
+if (x >= 2 and x < 3) or (x >= 0 and x <= 1) or (x >= -10 and x <= -2):
+    print(x, "appartient à l'intervalle I")
+else:
+    print(x, "n'appartient pas à l'intervalle I")
+
